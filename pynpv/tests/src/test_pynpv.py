bff982f04e6e8c861b13c21869638119b9f12671
Tom Vierjahn <vierjahn@vr.rwth-aachen.de>
2017-08-15 16:02:06 +0200

    Add a test to pynpv to demonstrate cout capturing

--- pynpv/tests/src/test_pynpv.py
+++ pynpv/tests/src/test_pynpv.py
@@ -19,5 +19,17 @@
 # limitations under the License.
 #-------------------------------------------------------------------------------
 
+import sys
+
 def test_pypvt():
     assert True
+
+def test_cout_capture(capsys):
+    print("hello")
+    sys.stderr.write("world\n")
+    out, err = capsys.readouterr()
+    assert out == "hello\n"
+    assert err == "world\n"
+    print ("next")
+    out, err = capsys.readouterr()
+    assert out == "next\n"
