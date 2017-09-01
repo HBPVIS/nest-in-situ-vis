71dbac16b57ee7da823c7abfa71c3660386af6c7
Tom Vierjahn <vierjahn@vr.rwth-aachen.de>
2017-09-02 00:23:59 +0200

    Make SharedMemoryAccess destructor virtual

--- niv/include/niv/shared_memory_access.hpp
+++ niv/include/niv/shared_memory_access.hpp
@@ -29,7 +29,7 @@ namespace niv {
 class SharedMemoryAccess : public SharedMemory {
  public:
   SharedMemoryAccess();
-  ~SharedMemoryAccess() = default;
+  virtual ~SharedMemoryAccess() = default;
 };
 
 }  // namespace niv
