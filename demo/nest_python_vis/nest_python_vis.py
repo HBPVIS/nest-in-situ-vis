3f8df7188acd173c2a340f28f3c2aedc9490b1a2
Tom Vierjahn <vierjahn@vr.rwth-aachen.de>
2017-09-05 17:08:09 +0200

    Add auto update to vis mockup

--- demo/nest_python_vis/nest_python_vis.py
+++ demo/nest_python_vis/nest_python_vis.py
@@ -3,15 +3,18 @@ import sys
 import pyniv
 
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QPushButton
+from PyQt5.QtCore import QTimer
 
 class MainWindow:
     def __init__(self):
         self.receiver = pyniv.ConduitReceiver()
         self.SetupWindow()
+        self.SetupUpdateTimer()
+        
 
     def SetupWindow(self):
         self.label = QLabel("V_m:")
-        self.value = QLabel("0.0")
+        self.value = QLabel("{:0.3f} mV".format(0.0))
 
         self.start_button = QPushButton("Start")
         self.start_button.clicked.connect(self.StartButtonClicked)
@@ -26,16 +29,24 @@ class MainWindow:
         self.window.setLayout(self.layout)
         self.window.show()
 
+    def SetupUpdateTimer(self):
+        self.update_timer = QTimer()
+        self.update_timer.timeout.connect(self.UpdateValue)
+
+
     def StartButtonClicked(self):
         self.start_button.setEnabled(False)
         self.receiver.Start()
+        self.update_timer.start(0.01)
         self.UpdateValue()
 
     def Show(self):
         self.window.show()
 
     def UpdateValue(self):
-        pass
+        self.value.setText("{:0.3f} mV".format(self.receiver.Get("V_m")))
+        self.value.update()
+        self.window.update()
 
 def main(argv):
     app = QApplication(argv)
