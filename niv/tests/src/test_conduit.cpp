326fc2ae315adab26bc26be346e969fff65caab3
Tom Vierjahn <vierjahn@vr.rwth-aachen.de>
2017-11-22 16:13:58 +0100

    Create a smoke test, indicating that conduit's #226 is fixed

--- niv/tests/src/test_conduit.cpp
+++ niv/tests/src/test_conduit.cpp
@@ -74,29 +74,47 @@ SCENARIO("conduit array leafs are compatible to std::vector", "[conduit]") {
   }
 }
 
-SCENARIO("node serialization works repeatedly using a node storage",
-         "[conduit]") {
-  GIVEN("a serialized node") {
-    niv::LocalNodeStorage storage;
-    storage.Store(testing::AnyNode());
+namespace {
 
-    WHEN("serialization is read into a second node") {
-      conduit::Node second_node;
-      storage.Read(&second_node);
+void SerializeConstRef(const conduit::Node& node, std::string* schema,
+                       std::vector<conduit::uint8>* bytes) {
+  conduit::Schema compact_schema;
+  node.schema().compact_to(compact_schema);
+  const std::string compact_schema_json(compact_schema.to_json());
+  schema->assign(compact_schema_json.begin(), compact_schema_json.end());
 
-      THEN("they are identical") {
-        REQUIRE_EQUAL_NODES(second_node, testing::AnyNode());
-      }
+  // intermediate vector not required in general, but our use case requires it
+  std::vector<conduit::uint8> data;
+  node.serialize(data);
+  bytes->assign(data.begin(), data.end());
+}
 
-      WHEN("the second node is serialized and read again") {
-        storage.Store(second_node);
-        conduit::Node third_node;
-        storage.Read(&third_node);
+}  // namespace
 
-        THEN("they are identical") {
-          REQUIRE_EQUAL_NODES(third_node, testing::AnyNode());
-        }
-      }
+SCENARIO("confirm that conduit issue #226 is not yet fixed", "[conduit]") {
+  INFO(
+      "This test's failing indicates that conduit issue #226 might have been "
+      "fixed.\n"
+      "Check https://github.com/LLNL/conduit/issues/226 \n"
+      "Also adjust niv::NodeStorage::Store(...) and "
+      "niv::NodeStorage::Update(...) \n"
+      "so that they not do superfluous copying anymore.");
+  GIVEN("a node that is serialized and read back") {
+    std::string schema;
+    std::vector<conduit::uint8> bytes;
+
+    SerializeConstRef(testing::AnyNode(), &schema, &bytes);
+
+    conduit::Node second_node;
+    second_node.set_data_using_schema(conduit::Schema(schema), bytes.data());
+
+    WHEN("the second node is serialized and read into a third node") {
+      SerializeConstRef(second_node, &schema, &bytes);
+
+      conduit::Node third_node;
+      third_node.set_data_using_schema(conduit::Schema(schema), bytes.data());
+
+      REQUIRE_FALSE(third_node.to_json() == testing::AnyNode().to_json());
     }
   }
 }
