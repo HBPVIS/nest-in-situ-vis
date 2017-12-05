91c729fbc2afca738d3dd5de0163fc9a3487311a
Tom Vierjahn <vierjahn@vr.rwth-aachen.de>
2017-12-05 06:58:47 +0100

    Cleanup vis demo preparing for new sync relay exchange

--- demo/nest_python_vis/nest_python_vis.py
+++ demo/nest_python_vis/nest_python_vis.py
@@ -28,59 +28,26 @@ from PyQt5.QtCore import QTimer
 
 class MainWindow:
     def __init__(self):
-        self.receiver = pyniv.ConduitReceiver()
+        self.receiver = pyniv.SynchronizedReceiver()
         self.SetupWindow()
-        self.SetupUpdateTimer()
-        
 
     def SetupWindow(self):
-        self.label_V_m = QLabel("V_m:")
-        self.value_V_m = QLabel("{:0.3f} mV".format(0.0))
-
-        self.label_g_ex = QLabel("g_ex:")
-        self.value_g_ex = QLabel("{:0.3f}".format(0.0))
-
-        self.label_g_in = QLabel("g_ex:")
-        self.value_g_in = QLabel("{:0.3f}".format(0.0))
-
-        self.start_button = QPushButton("Start")
-        self.start_button.clicked.connect(self.StartButtonClicked)
+        self.visualize_button = QPushButton("Visualize")
+        self.visualize_button.clicked.connect(self.VisualizeButtonClicked)
 
         self.layout = QGridLayout()
-        self.layout.addWidget(self.label_V_m, 0, 0)
-        self.layout.addWidget(self.value_V_m, 0, 1)
-        self.layout.addWidget(self.label_g_ex, 1, 0)
-        self.layout.addWidget(self.value_g_ex, 1, 1)
-        self.layout.addWidget(self.label_g_in, 2, 0)
-        self.layout.addWidget(self.value_g_in, 2, 1)
  
-        self.layout.addWidget(self.start_button, 3, 0, 1, 2)
+        self.layout.addWidget(self.visualize_button, 0, 0, 1, 1)
 
         self.window = QWidget()
         self.window.setLayout(self.layout)
         self.window.show()
 
-    def SetupUpdateTimer(self):
-        self.update_timer = QTimer()
-        self.update_timer.timeout.connect(self.UpdateValue)
-
-
-    def StartButtonClicked(self):
-        self.start_button.setEnabled(False)
-        self.receiver.Start()
-        self.update_timer.start(0.01)
-        self.UpdateValue()
+    def VisualizeButtonClicked(self):
+        print("FOO")
 
     def Show(self):
         self.window.show()
-
-    def UpdateValue(self):
-        self.value_V_m.setText("{:0.3f} mV".format(self.receiver.Get("multimeter_conduit-2-0/V_m")))
-        self.value_g_ex.setText("{:0.3f}".format(self.receiver.Get("multimeter_conduit-2-0/g_ex")))
-        self.value_g_in.setText("{:0.3f}".format(self.receiver.Get("multimeter_conduit-2-0/g_in")))
-
-        self.value_V_m.update()
-        self.window.update()
 
 def main(argv):
     app = QApplication(argv)
